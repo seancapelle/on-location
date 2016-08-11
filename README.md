ac4f1861df2add2aafc962b2c654b5f1f2375153
Sean Capelle <seancapelle@gmail.com>
2016-08-11 19:04:52 -0400

    moved files

---
+++ README.md
@@ -0,0 +1,49 @@
+# Onlocation     
+![group-logo](https://cloud.githubusercontent.com/assets/18410732/17531845/b185b96e-5e4b-11e6-905f-9bf29c90b56b.png)
+
+https://safe-oasis-68574.herokuapp.com
+
+**# What's this website about?**
+Onlocation is a searchable database by country and state (for USA) which movies were filmed.
+Once movie is selected the website will show a poster of the movie, youtube trailer and synopsis (plot).
+
+![image](https://cloud.githubusercontent.com/assets/18410732/17531551/54057474-5e4a-11e6-9b5f-483b0303a6c6.png)
+
+# Technologies used:
+
+- JavaScript
+
+- HTML
+
+- CSS3
+
+- GITHUB
+
+- BOOTSTRAP
+
+- GOOGLE FONTS
+
+- HEROKU
+
+- GIT
+
+- SUBLIME TEXT EDITOR
+
+- TERMINAL (Mac Users)
+
+- MYAPIFILMS
+
+- YOUTUBE TRAILERS API
+
+- GOOGLE MAPS API
+
+- AJAX
+
+- JSON
+
+# Onlocation contributors:
+
+https://github.com/seancapelle
+https://github.com/Nataleep
+https://github.com/awesome792
+
---
+++ assets/javascript/app2.js
@@ -0,0 +1,92 @@
+//Functions
+//Grabs movie's data
+var movieSearch = function(moviePick){
+
+    console.log("In movieSearch");
+
+    //Retrieve movePick from localStorage
+    var storedMovie = localStorage.getItem('movie');
+    
+    console.log(storedMovie);
+
+    //API URL with user's movie
+    var queryURL = "http://api.myapifilms.com/imdb/idIMDB?title=" + storedMovie + "&token=66a93a66-d0a3-4bcf-b682-329ad22dc9c3";
+
+    //Ajax call
+    $.ajax({url: queryURL, method: 'GET'})
+        //Ajax response
+        .done(function(response) {
+
+            console.log(response);
+
+            var image = response.data.movies[0].urlPoster;
+
+            //Create movie object
+            var movie = {
+                title: response.data.movies[0].title,
+                year: response.data.movies[0].year,
+                rating: response.data.movies[0].rated,
+                plot: response.data.movies[0].simplePlot,
+                poster: response.data.movies[0].urlPoster,
+                location: response.data.movies[0].filmingLocations.toString(), 
+            };
+
+            console.log(movie.location);
+            //Push movie properties
+            $('#title').append(movie.title);
+            $('#year').append(movie.year);
+            $('#rating').append(movie.rating);
+            $('#plot').append(movie.plot);
+            $('#poster').append('<img src =' + movie.poster + '</>');
+        
+        geoCode(movie.location);
+    });
+}
+            //David's API call
+            // $.ajax({
+            //     url: 'https://temp-example.herokuapp.com/locations.html',
+            //     type: 'GET',
+            //     success: function(data) {
+            //         // var htmlText = $(data).find('a');
+            //         var htmlText = $(data).find('#filming_locations_content .sodavote > dt > a');
+            //         $("#filming_locations_content").append(htmlText);
+            //         $.each(htmlText, function(key, value) {
+            //             console.log(key + ": " + $(value).text());
+            //         });
+
+            //     }
+            // });
+
+//Turn movie.location into longitude and latitude
+var geoCode = function(location){
+
+var geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyBftOH0wF8TmozxEQNEfSFizvuk_LFtk0k";
+//Ajax call
+    $.ajax({url: geoURL, method: 'GET'})
+        //Ajax response
+        .done(function(response) {
+
+            console.log(response);
+
+            var lat = response.results[0].geometry.location.lat;
+            var long = response.results[0].geometry.location.lng;
+
+            console.log(lat);
+            console.log(long);
+            
+            initMap(long, lat);
+    });
+
+}
+//Create map based on lat and long
+var initMap = function(long, lat) {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('googlemap'), {
+          center: {lat: lat, lng: long},
+          scrollwheel: true,
+          zoom: 8
+        });
+ };
+
+//Calls the function
+ movieSearch();
