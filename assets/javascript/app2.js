f7d3de804f13e8f59b4d0f198b9b9b45d2072e7b
Sean Capelle <seancapelle@gmail.com>
2016-08-13 09:45:15 -0400

    Final push

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -118,3 +118,216 @@ $(document).ready(function() {
 //Calls the function
  movieSearch();
 });
+
+
+// //Variables 
+
+// var movieList = [];
+
+// //Functions
+
+// //Search by movie
+// //Grabs movie's data
+// var movieSearch = function(){
+
+// console.log("In movieSearch");
+
+// var storedMovie = '';  
+
+// var database = firebase.database();
+    
+//      database.ref("currentmoviePick").on("value", function(snapshot) {
+
+//                 // Print the initial data to the console.
+//                 console.log(snapshot.val());
+
+//                 // Log the value of the various properties
+//                 console.log(snapshot.val().moviePick);
+                
+//                 storedMovie = snapshot.val().moviePick;
+
+//     //API URL with user's movie
+//     var queryURL = "http://api.myapifilms.com/imdb/idIMDB?title=" + storedMovie + "&token=66a93a66-d0a3-4bcf-b682-329ad22dc9c3";
+
+//     //Ajax call
+//     $.ajax({url: queryURL, method: 'GET'})
+//         //Ajax response
+//         .done(function(response) {
+
+//             console.log(response);
+
+//             //Create movie object
+//             var movie = {
+//                 title: response.data.movies[0].title,
+//                 year: response.data.movies[0].year,
+//                 rating: response.data.movies[0].rated,
+//                 plot: response.data.movies[0].simplePlot,
+//                 poster: response.data.movies[0].urlPoster,
+//                 location: response.data.movies[0].filmingLocations.toString(), 
+//             };
+
+//             console.log(movie.location);
+//                 //Push movie properties
+//                 $('#title').append(movie.title);
+//                 $('#year').append(movie.year);
+//                 $('#rating').append(movie.rating);
+//                 $('#plot').append(movie.plot);
+//                 $('#poster').append('<img src =' + movie.poster + '</>');
+        
+//         geoCode(movie.location);
+//     });
+// });
+
+// //Turn movie.location into longitude and latitude
+// var geoCode = function(location){
+
+// var geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyBftOH0wF8TmozxEQNEfSFizvuk_LFtk0k";
+// //Ajax call
+//     $.ajax({url: geoURL, method: 'GET'})
+//         //Ajax response
+//         .done(function(response) {
+
+//             console.log(response);
+
+//             var lat = response.results[0].geometry.location.lat;
+//             var long = response.results[0].geometry.location.lng;
+
+//             console.log(lat);
+//             console.log(long);
+            
+//             initMap(long, lat);
+//     });
+
+// }
+// //Create map based on lat and long
+// var initMap = function(long, lat) {
+
+//         var myLatLng = { lat: lat, lng: long};
+
+//         // Create a map object and specify the DOM element for display.
+//         var map = new google.maps.Map(document.getElementById('googlemap'), {
+//           center: myLatLng,
+//           // {lat: lat, lng: long},
+//           scrollwheel: true,
+//           zoom: 8
+//         });
+
+//         var marker = new google.maps.Marker({
+//         position: myLatLng,
+//         map: map,
+//         // title: location
+//   });
+//  };
+
+// $(document).ready(function() { 
+
+// //Calls the function
+//  movieSearch();
+// });
+
+// // //Functions
+
+// // //Grabs movie's data
+// // var movieSearch = function(){
+
+// // console.log("In movieSearch");
+
+// // var storedMovie = '';  
+
+// // var database = firebase.database();
+    
+// //      database.ref("currentmoviePick").on("value", function(snapshot) {
+
+// //                 // Print the initial data to the console.
+// //                 console.log(snapshot.val());
+
+// //                 // Log the value of the various properties
+// //                 console.log(snapshot.val().moviePick);
+                
+// //                 storedMovie = snapshot.val().moviePick;
+
+// //     //API URL with user's movie
+// //     var queryURL = "http://api.myapifilms.com/imdb/idIMDB?title=" + storedMovie + "&token=66a93a66-d0a3-4bcf-b682-329ad22dc9c3";
+
+// //     console.log(queryURL);
+// //         //Ajax call
+// //         $.ajax({url: queryURL, method: 'GET'})
+// //             //Ajax response
+// //             .done(function(response) {
+
+// //                 console.log(response);
+
+// //                 var image = response.data.movies[0].urlPoster;
+
+// //                 //Create movie object
+// //                 var movie = {
+// //                     title: response.data.movies[0].title,
+// //                     year: response.data.movies[0].year,
+// //                     rating: response.data.movies[0].rated,
+// //                     plot: response.data.movies[0].simplePlot,
+// //                     poster: response.data.movies[0].urlPoster,
+// //                     location: response.data.movies[0].filmingLocations.toString(), 
+// //                 };
+
+// //                 console.log(movie.location);
+// //                 //Push movie properties
+// //                 $('#title').append(movie.title);
+// //                 $('#year').append(movie.year);
+// //                 $('#rating').append(movie.rating);
+// //                 $('#plot').append(movie.plot);
+// //                 $('#poster').append('<img src =' + movie.poster + '</>');
+            
+       
+// //             geoCode(movie.location);
+
+// //         });
+
+// // }
+            
+
+// // //Turn movie.location into longitude and latitude
+// // var geoCode = function(location){
+
+
+// // var geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyBftOH0wF8TmozxEQNEfSFizvuk_LFtk0k";
+// // //Ajax call
+// //     $.ajax({url: geoURL, method: 'GET'})
+// //         //Ajax response
+// //         .done(function(response) {
+
+// //             console.log(response);
+
+// //             var lat = response.results[0].geometry.location.lat;
+// //             var long = response.results[0].geometry.location.lng;
+
+// //             console.log(lat);
+// //             console.log(long);
+            
+// //             initMap(long, lat, location);
+// //     });
+
+// // //Create map based on lat and long
+// // var initMap = function(long, lat, location) {
+
+// //         var myLatLng = { lat: lat, lng: long};
+
+// //         // Create a map object and specify the DOM element for display.
+// //         var map = new google.maps.Map(document.getElementById('googlemap'), {
+// //           center: myLatLng,
+// //           // {lat: lat, lng: long},
+// //           scrollwheel: true,
+// //           zoom: 8
+// //         });
+
+// //         var marker = new google.maps.Marker({
+// //         position: myLatLng,
+// //         map: map,
+// //         title: location
+// //   });
+// //  };
+// // //Page ready
+// // $(document).ready(function() { 
+
+// // //Calls the function
+// //  movieSearch();
+// // });
